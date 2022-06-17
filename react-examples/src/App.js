6ac8ca48e5ae03e273b6e7b46eb79107d2b03169
tomatovine1 <hesmaeilz01@gmail.com>
2022-06-17 11:31:25 -0400

    using lists in HTML to reduce rerending freq

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,24 +1,36 @@
 // App.js
 import React, { Component } from 'react';
-class Collatz extends Component { render() {
-    const { number } = this.props;
-        // WARNING: This code reruns with every rendering
-        let x = number;
-        let outputString = x.toString();
-        while (typeof x == 'number' && x !== 1) {
-            if ((x % 2) === 1) { x = 3*x + 1;
-            } else {
-                x = x/2;
-                };
-                outputString += ' ' + x; };
-                return (
-                    <div className="Collatz">
-                        <h2>Collatz Sequence for {number}</h2>
-                        <p>{ outputString }</p> 
-                    </div>
-                );
-            }
+class Collatz extends Component { constructor(props)
+    {
+    super(props)
+     let x = this.props.number;
+     let outputArr = [x]
+    while (typeof x == 'number' && x !== 1) {
+        if ((x % 2) === 1) { 
+                x = 3*x + 1;
+        } 
+        else {
+            x = x/2;
+        };
+             outputArr.push(x); 
+            };
+            this.outputArr = outputArr;
         }
+             render() {
+                 const { number } = this.props;
+                 const outputArr = this.outputArr;
+                 return( 
+                <div className="Collatz">
+                     <h2>Collatz Sequence for {number}</h2>
+                    <ul>{
+                     outputArr.map((x,ind)=>{ return <li key = {ind} > {x} 
+                     </li>})
+                     }
+                     </ul> 
+                </div>
+            );
+            }
+    }
 
 class App extends Component { 
     render() {
